6557369bbe2402c504dca0074079364f69f63824
rethavanderwalt <42917205+rethavanderwalt@users.noreply.github.com>
2019-07-09 10:35:04 +1200

    Like + Dislike Buttons
    Both buttons count number of clicks

--- scripts/ratings.js
+++ scripts/ratings.js
@@ -1,5 +1,11 @@
 var likes = 0;
-    function onClick() {
+    function likeVideo() {
         likes += 1;
         document.getElementById("likes").innerHTML = likes;
-    };
+    }
+
+var dislikes = 0;
+    function dislikeVideo() {
+        dislikes += 1;
+        document.getElementById("dislikes").innerHTML = dislikes;
+    }
