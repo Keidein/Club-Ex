67ea08141a30d469692f6efe023d60cd7d8d0fcf
rethavanderwalt <42917205+rethavanderwalt@users.noreply.github.com>
2019-07-10 12:25:49 +1200

    Like x1 per user
    Updated so like button can only increment by 1 for each user

--- scripts/ratings.js
+++ scripts/ratings.js
@@ -1,8 +1,41 @@
-var likes = 0;
-    function likeVideo() {
-        likes += 1;
+
+
+
+// COOKIES
+
+function setLike(articleID) {
+    document.cookie=articleID;
+}
+
+function hasLiked(articleID) {
+    var cookies=document.cookie.split(';');
+    for (var i=0;i<cookies.length;i++) {
+        var cookie=cookies[i].split('=');
+        if (cookie[0]==articleID) return true;
+    }
+    return false;
+}
+
+var button=document.getElementById('likeBtn');
+
+function likedVideo() {
+    var articleID='articleID';
+    if (!hasLiked(articleID)) {
+        setLike(articleID);
+    } else {
+        alert('You cant like or dislike an article twice');
+    }
+}
+
+
+function likeVideo() {
+    var likes = 0;
+    likedVideo();
+    if (hasLiked != true) {
+        likes ++;
         document.getElementById("likes").innerHTML = likes;
     }
+}
 
 var dislikes = 0;
     function dislikeVideo() {
