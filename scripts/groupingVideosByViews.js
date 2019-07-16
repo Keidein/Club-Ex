cfc01493ee6ad63138203acc960786238db0e368
r3d-fan6 <dtetheridge@gmail.com>
2019-07-16 12:45:29 +1200

    sorting alg for popularity

---
+++ scripts/groupingVideosByViews.js
@@ -0,0 +1,41 @@
+$(document).ready(() => {
+    setTimeout(() => {
+        var li = document.getElementsByTagName("li"),
+    wrapper = document.getElementById("wrapper"),
+    newSort = [];
+
+    for (let i = 0; i < li.length-1; i++) {
+
+        var tag1 = li[i].id*1,
+        tag2 = li[i + 1].id*1;
+
+        if (!isNaN(tag1)) {
+            if (tag1 > tag2) {
+                newSort.push(li[i]);
+                li[i].remove();
+            }
+            if (tag1 < tag2) {
+                newSort.push(li[i + 1]);
+                li[i + 1].remove();
+            }
+        }
+    }
+
+    console.log(li, newSort);
+
+    list = li.length;
+
+    if (list > 0) {
+        for (var i = 0; i < list; i++) {
+            newSort.push(li[0]);
+            li[0].remove();
+        }
+    }
+
+    console.log(li, newSort);
+
+    for (let i = 0; i < newSort.length; i++) {
+        wrapper.innerHTML = wrapper.innerHTML + newSort[i].outerHTML;
+    }
+    }, 3000);
+});
