e73bda517df2decd7f718bd6a708bcebd21d541b
Retha <91030418@mail.computerpower.ac.nz>
2019-07-17 10:00:43 +1200

    Login & Signup css update

--- scripts/login.js
+++ scripts/login.js
@@ -7,16 +7,19 @@ function createLogin() {
     passwordCheck = false;
     emailCheck = false;
 
-    if (username.value.length <= 5) {
-        document.getElementById("username-error").style.visibility = "visible";
+    if (username.value.length < 5) {
+        document.getElementById("username").placeholder = "Error With Username";
+        document.getElementById("username").style.border="3px solid #FB3640";
+        alert("Error: There has been an error with your registration attempt. Please review your username.");
         usernameCheck = false;
     } else {
-        document.getElementById("username-error").style.visibility = "hidden";
         usernameCheck = true;
     }
 
     if (password.value.length < 5) {
-        document.getElementById("password-error").style.visibility = "visible";
+        document.getElementById("password").placeholder = "Error With Password";
+        document.getElementById("password").style.border="3px solid #FB3640";
+        alert("Error: There has been an error with your registration attempt. Please review your password.");
         passwordCheck = false;
     } else {
         document.getElementById("password-error").style.visibility = "hidden";
@@ -25,10 +28,11 @@ function createLogin() {
 
     var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!regex.test(email.value)) {
-        document.getElementById("email-error").style.visibility = "visible";
+        document.getElementById("email").placeholder = "Error With Email";
+        document.getElementById("email").style.border="3px solid #FB3640";
+        alert("Error: There has been an error with your registration attempt. Please review your email.");
         emailCheck = false;
     } else {
-        document.getElementById("email-error").style.visibility = "hidden";
         emailCheck = true;
     }
 
@@ -51,20 +55,27 @@ function login() {
     var username = document.getElementById("username");
     var password = document.getElementById("password");
 
-    if (username.value <= 5 || !(username.value in data)) {
-        document.getElementById("username-error").style.visibility = "visible";
+    if (username.value < 5 || !(username.value in data)) {
+        document.getElementById("username").placeholder = "Error With Username";
+        document.getElementById("username").style.border="3px solid #FB3640";
         usernameCheck = false;
     } else {
-        document.getElementById("username-error").style.visibility = "hidden";
         usernameCheck = true;
     }
 
-    if (password.value <= 5 || password.value != data[username.value]) {
-        document.getElementById("password-error").style.visibility = "visible";
+    if (password.value < 5 || password.value != data[username.value]) {
+        document.getElementById("password").placeholder = "Error With Password";
+        document.getElementById("password").style.border="3px solid #FB3640";
         passwordCheck = false;
     } else {
         document.getElementById("password-error").style.visibility = "hidden";
         passwordCheck = true;
+    }
+
+    if ( usernameCheck == false ) {
+        alert("Incorrect username or password.")
+    } else if (passwordCheck == false) {
+        alert("Incorrect username or password.")
     }
 
     if (usernameCheck && passwordCheck) {
@@ -72,4 +83,27 @@ function login() {
             location.href = "./grouping.html";
         }
     }
+
+
+    /*
+    if (username.value <= 5 || !(username.value in data)) {
+        document.getElementById("username-error").style.visibility = "visible";
+        usernameCheck = false;
+    } else {
+        document.getElementById("username-error").style.visibility = "hidden";
+        usernameCheck = true;
+    }
+    if (password.value <= 5 || password.value != data[username.value]) {
+        document.getElementById("password-error").style.visibility = "visible";
+        passwordCheck = false;
+    } else {
+        document.getElementById("password-error").style.visibility = "hidden";
+        passwordCheck = true;
+    }
+    if (usernameCheck && passwordCheck) {
+        if (username.value in data && password.value == data[username.value]) {
+            location.href = "./grouping.html";
+        }
+    }
+    */
 }
